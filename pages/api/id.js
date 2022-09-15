822ac51ae6ad9854383f57325c5057dabc7450fe
RICO <ricoyou99@gmail.com>
2022-09-15 20:22:53 +0700

    locale v-1.1.1

--- pages/api/id.js
+++ pages/api/id.js
@@ -2,13 +2,30 @@ import moment from 'moment-timezone';
 
 export default function handler(req, res) {
   const data_time = 1662981377 * 1000;
-  moment.locale('id');
+
+  // format months
+  moment.updateLocale('en', {
+    months: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'Augustussss',
+      'Septemberer',
+      'October',
+      'November',
+      'December',
+    ],
+  });
+
   const default_date = moment(data_time)
     .tz('Asia/Jakarta')
     .format('h:mm:ss a, dddd MMMM Do YYYY');
   const full_date = moment(data_time)
     .tz('Asia/Jakarta')
-    .locale('id')
     .format('H:mm:ss a, dddd MMMM Do YYYY');
   const current_date = moment()
     .tz('Asia/Bangkok')
