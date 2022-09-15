ff782d09c1facab18e99f183b2e4440bd2332e59
RICO <ricoyou99@gmail.com>
2022-09-15 19:43:05 +0700

    locale v-1.0.1

--- pages/api/id.js
+++ pages/api/id.js
@@ -1,11 +1,16 @@
-import moment from 'moment/moment';
+import moment from 'moment-timezone';
 
 export default function handler(req, res) {
   const data_time = 1662981377 * 1000;
   moment.locale('id');
   const default_date = moment(data_time).format('h:mm:ss a, MMMM Do YYYY');
   const full_date = moment(data_time).format('H:mm:ss a, MMMM Do YYYY');
+  const current_date = moment()
+    .tz('Asia/Bangkok')
+    .format('H:mm:ss a, MMMM Do YYYY');
   res.status(200).json({
+    locale_date: moment.tz.guess(),
+    current_date,
     default_date,
     full_date,
   });
